267062aa5115eee4aa82c7d8fc8e60193811ae99
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2023-05-10 23:07:30 -0400

    Add files via upload

---
+++ get_wastewater_change_cdc.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# make sure to install these packages before running:
+# pip install pandas
+# pip install sodapy
+
+import pandas as pd
+from sodapy import Socrata
+
+# Unauthenticated client only works with public data sets. Note 'None'
+# in place of application token, and no username or password:
+client = Socrata("data.cdc.gov", None)
+
+# Example authenticated client (needed for non-public datasets):
+# client = Socrata(data.cdc.gov,
+#                  MyAppToken,
+#                  username="user@example.com",
+#                  password="AFakePassword")
+
+# First 200000000 results, returned as JSON from API / converted to Python list of
+# dictionaries by sodapy.
+results = client.get("2ew6-ywp6", limit=200000000)
+
+# Convert to pandas DataFrame
+results_df = pd.DataFrame.from_records(results)
+results_df.to_csv('cdc_wastewater_metric_perc_change.csv')
