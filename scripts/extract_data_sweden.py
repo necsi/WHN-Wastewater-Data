bb9648b8a73ac8cb6216ada8ffd2558440573332
Yingnan Cui <yingn.cui@gmail.com>
2023-05-17 10:31:58 -0400

    Adding GitHub workflow for Sweden wastewater data retrieval.

--- scripts/extract_data_sweden.py
+++ scripts/extract_data_sweden.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sqlite3 as sl
 
 
-df = pd.read_csv('../data/Sweden/Relative-copy-number-of-SARS-CoV-2-to-PPMoV-in-Uppsala-wastewater.csv')
+df = pd.read_csv('../data/Sweden/Sweden_ww.csv')
 df = df.rename(columns={'relative_copy_number': 'rel_copy_number',
                         'infA-gene cn per PMMoV cn x 10000': 'infA',
                         'infB-gene cn per PMMoV cn x 10000': 'infB'})
