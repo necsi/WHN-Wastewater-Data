58a004f9e20ef3baa1a1ef223a33d752a4276978
Yingnan Cui <yingn.cui@gmail.com>
2023-05-17 10:50:56 -0400

    Netherlands already provides .csv file. Fetch CSV file instead.

--- scripts/get_data_Netherlands.py
+++ scripts/get_data_Netherlands.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-df = pd.read_json('https://data.rivm.nl/covid-19/COVID-19_rioolwaterdata.json')
+df = pd.read_csv('https://data.rivm.nl/covid-19/COVID-19_rioolwaterdata.csv')
 
-df.to_csv('data/Netherlands/nl_wastewater_data.csv')
-
+df.to_csv('../data/Netherlands/nl_wastewater_data_test.csv')
