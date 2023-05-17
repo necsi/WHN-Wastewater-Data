9ee5d79095c87919a90cbf394d8ac82ef8e8106d
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2023-05-16 23:35:39 -0400

    Update get_data_Netherlands.py

--- scripts/get_data_Netherlands.py
+++ scripts/get_data_Netherlands.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 df = pd.read_json('https://data.rivm.nl/covid-19/COVID-19_rioolwaterdata.json')
 
-df.to_csv('nl_wastewater_data.csv')
+df.to_csv('../data/Netherlands/nl_wastewater_data.csv')
