1b2fea35a521b91b4d12a5535323ba67e75ef8cc
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2023-10-06 00:23:59 -0400

    Update canada_get_data.py

--- scripts/canada_get_data.py
+++ scripts/canada_get_data.py
@@ -8,33 +8,39 @@ shedding = pd.read_csv('FecalSheddingModel.csv', index_col=0)
 col_names_ww_VCH_F = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQBi1bvkrF8c_46Ak5exKm07Nqej7Es1N-HHh9LHuR6M-tOF1H46H1ztCB5nSlPb_mJ7uGdsjA4proZ/pub?gid=1782200355&single=true&output=csv', header=None, nrows=1, usecols=range(6), skiprows=1).iloc[0]
 col_names_ww_VIHA = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQBi1bvkrF8c_46Ak5exKm07Nqej7Es1N-HHh9LHuR6M-tOF1H46H1ztCB5nSlPb_mJ7uGdsjA4proZ/pub?gid=1135929066&single=true&output=csv', header=None, nrows=1, usecols=range(4), skiprows=1).iloc[0]
 col_names_ww_IH = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQBi1bvkrF8c_46Ak5exKm07Nqej7Es1N-HHh9LHuR6M-tOF1H46H1ztCB5nSlPb_mJ7uGdsjA4proZ/pub?gid=168719565&single=true&output=csv', header=None, nrows=1, usecols=range(4), skiprows=1).iloc[0]
+col_names_ww_NH = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQBi1bvkrF8c_46Ak5exKm07Nqej7Es1N-HHh9LHuR6M-tOF1H46H1ztCB5nSlPb_mJ7uGdsjA4proZ/pub?gid=974971806&single=true&output=csv', header=None, nrows=1, usecols=range(2), skiprows=1).iloc[0]
 
 # Read the rest of the data, skipping the first two rows
 ww_VCH_F = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQBi1bvkrF8c_46Ak5exKm07Nqej7Es1N-HHh9LHuR6M-tOF1H46H1ztCB5nSlPb_mJ7uGdsjA4proZ/pub?gid=1782200355&single=true&output=csv', header=None, skiprows=2, usecols=range(6), names=col_names_ww_VCH_F)
 ww_VIHA = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQBi1bvkrF8c_46Ak5exKm07Nqej7Es1N-HHh9LHuR6M-tOF1H46H1ztCB5nSlPb_mJ7uGdsjA4proZ/pub?gid=1135929066&single=true&output=csv', header=None, skiprows=2, usecols=range(4), names=col_names_ww_VIHA)
 ww_IH = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQBi1bvkrF8c_46Ak5exKm07Nqej7Es1N-HHh9LHuR6M-tOF1H46H1ztCB5nSlPb_mJ7uGdsjA4proZ/pub?gid=168719565&single=true&output=csv', header=None, skiprows=2, usecols=range(4), names=col_names_ww_IH)
+ww_NH = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQBi1bvkrF8c_46Ak5exKm07Nqej7Es1N-HHh9LHuR6M-tOF1H46H1ztCB5nSlPb_mJ7uGdsjA4proZ/pub?gid=974971806&single=true&output=csv', header=None, skiprows=2, usecols=range(2), names=col_names_ww_NH)
 
 # Change first column name to date
 ww_VCH_F.rename(columns={ww_VCH_F.columns[0]:'Date'}, inplace=True)
 ww_VIHA.rename(columns={ww_VIHA.columns[0]:'Date'}, inplace=True)
 ww_IH.rename(columns={ww_IH.columns[0]:'Date'}, inplace=True)
+ww_NH.rename(columns={ww_NH.columns[0]:'Date'}, inplace=True)
 
 # Change date column to datetime format
 ww_VCH_F['Date'] = pd.to_datetime(ww_VCH_F['Date'])
 ww_VIHA['Date'] = pd.to_datetime(ww_VIHA['Date'])
 ww_IH['Date'] = pd.to_datetime(ww_IH['Date'])
+ww_NH['Date'] = pd.to_datetime(ww_NH['Date'])
 
 # Set date as index
 ww_VCH_F.set_index('Date', inplace=True)
 ww_VIHA.set_index('Date', inplace=True)
 ww_IH.set_index('Date', inplace=True)
+ww_NH.set_index('Date', inplace=True)
 
-# Bring all values in VIHA and IH to billion gc / day from trillion gc / day
+# Bring all values in VIHA, IH and NH to billion gc / day from trillion gc / day
 ww_VIHA = ww_VIHA * 1000
 ww_IH = ww_IH * 1000
+ww_NH = ww_NH * 1000
 
 # Concatenate all dataframes
-ww = pd.concat([ww_VCH_F, ww_VIHA, ww_IH], axis=1)
+ww = pd.concat([ww_VCH_F, ww_VIHA, ww_IH, ww_NH], axis=1)
 
 # Delete all rows at the end that contain only NaN entries
 last_valid_index = ww.dropna(how='all').last_valid_index()
@@ -43,5 +49,5 @@ ww = ww.loc[:last_valid_index]
 # Interpolate missing values in each column
 ww = ww.interpolate(method='linear', axis=0)
 
-# Save as csv
+# Save to csv
 ww.to_csv('data/Canada/ww_BC_Canada.csv')
