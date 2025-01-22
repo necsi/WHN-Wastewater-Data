0c83be88d5d46fa7b54d6dd54348757030654b05
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2025-01-22 11:36:05 -0500

    Update ww_factor_NWSS_min.py

--- ww_factor_NWSS_min.py
+++ ww_factor_NWSS_min.py
@@ -45,6 +45,18 @@ nwss_data['Population'] = pd.to_numeric(nwss_data['Population'], errors='coerce'
 # Remove negative values
 nwss_data['gc/capita/day'] = nwss_data['gc/capita/day'].clip(lower=0)
 
+# Test Jan 18 2025 regarding 5 outlier plants in Erie County, NY - reverted until we made a decision in the team, not sure what we are looking at
+# Define the list of outlier treatment plant IDs
+outlier_plants = ['NWSS_ny_1012_Treatment plant_raw wastewater', 'NWSS_ny_1013_Treatment plant_raw wastewater', 'NWSS_ny_1000_Treatment plant_raw wastewater', 'NWSS_ny_2178_Treatment plant_raw wastewater', 'NWSS_ny_998_Treatment plant_raw wastewater']
+
+# Filter out the outliers from the dataset
+nwss_data = nwss_data[~nwss_data['key_plot_id'].isin(outlier_plants)]
+
+# Confirm the removal of the outliers
+nwss_data = nwss_data.reset_index(drop=True)
+
+# Test Jan 18 2025 end
+
 # Step to identify outliers based on comparison with surrounding values
 nwss_data['rolling_median'] = nwss_data.groupby('key_plot_id')['gc/capita/day'].transform(lambda x: x.rolling(window=3, center=True).median())
 
